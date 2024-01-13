1f4fc4a64269234384bd621e7c6a9a52f792206b
pratikkabra143 <98254405+pratikkabra143@users.noreply.github.com>
2024-01-13 20:43:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return 'Dead Weight.'
+        return 'Grim Reaper 💀'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
